91d820318673555ce226f13faab169cad4d25d6f
Md. Jubayer <mdjubayer247@gmail.com>
2021-03-22 19:04:16 +0600

    Update BlueToothESP32ConnectWifi.ino
    Serial cred input added

--- BlueToothESP32ConnectWifi.ino
+++ BlueToothESP32ConnectWifi.ino
@@ -28,12 +28,12 @@ void setup() {
   NVS.begin("storage");
   is_wifi_cred_exists = hasWifiCredentials();
   if (!is_wifi_cred_exists) {
-    setSSIDFromBluetooth();
+    setSSIDFromPCSerial();
   }
   String WIFI_SSID = NVS.getString(nvs_ssid_key);
   String  WIFI_PASSWORD = NVS.getString(nvs_wifi_pswd_key);
-  Serial.println("connecting to ssid "+WIFI_SSID);
-  Serial.println("using password "+WIFI_PASSWORD);
+  Serial.println("connecting to ssid " + WIFI_SSID);
+  Serial.println("using password " + WIFI_PASSWORD);
   WiFi.begin(WIFI_SSID.c_str(), WIFI_PASSWORD.c_str());
   byte wait_counter = 0;
   while (WiFi.status() != WL_CONNECTED)
@@ -64,8 +64,16 @@ void loop() {
     ESP.restart();
   }
 
+  if (Serial.available() > 0) {
+    byte r = Serial.read();
+    if (char(r) == '0') {
+      Serial.println("Resetting the device");
+      eraseWifiCredentials();
+      digitalWrite(LED_BUILTIN, LOW);
+      ESP.restart();
+    }
+  }
   delay(500);
-
 }
 
 
@@ -122,6 +130,55 @@ void setSSIDFromBluetooth() {
               res = NVS.setString(nvs_wifi_pswd_key, message);
               if (res) {
                 ESP_BT.println("success");
+                res = false;
+              }
+            }
+            Serial.println(message);
+            message = "";
+            bluetooth_received_parameter++;
+          }
+        }
+        delay(20);
+      }
+    }
+  }
+}
+
+void setSSIDFromPCSerial() {
+  bluetooth_received_parameter = 0;
+  Serial.println("Enter your SSID");
+  while (true) {
+    if (Serial.available() > 0) {
+      continue_input = true;
+    }
+    delay(50);
+    if (continue_input) {
+      while (bluetooth_received_parameter < 3) {
+        if (bluetooth_received_parameter == 2) {
+          ESP.restart();
+          break;
+        }
+        if (Serial.available() > 0) {
+          byte r = Serial.read();
+          if (char(r) != '\n') {
+            if (r != 13 && r != 10 && char(r) != '\0') {
+              message += char(r);
+            }
+          }
+          else {
+            if (bluetooth_received_parameter == 0) {
+              res = NVS.setString(nvs_ssid_key, message);
+              if (res) {
+                Serial.println("success");
+                res = false;
+              }
+              Serial.println("Enter your password");
+            }
+
+            if (bluetooth_received_parameter == 1 ) {
+              res = NVS.setString(nvs_wifi_pswd_key, message);
+              if (res) {
+                Serial.println("success");
                 res = false;
               }
             }
